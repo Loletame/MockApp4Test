5946ab0d7d94b210a64bc758c570092ba0b53f7c
Loletame <loletame0100@gmail.com>
2024-05-13 17:47:21 -0300

    Commit def

---
+++ Back-end/src/models/user.js
@@ -0,0 +1,53 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../db/config.js';
+
+
+
+class User extends Model { }
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: false
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: false
+    },
+    age: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    }
+   },
+    {
+        sequelize,
+        modelName: 'User'
+    });
+      
+
+User.sync()
+    .then(() => {
+        console.log('La tabla de usuarios ha sido creada');
+    })
+    .catch((error) => {
+        console.error('Error al crear la tabla de usuarios: ', error);
+    });
+
+export default User;
---
+++ Back-end/src/routes/login.routes.js
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import { loginUser,createUser, getAllUsers } from "../controllers/user.controller.js";
+import { check } from "express-validator";
+import { validarCampos } from "../middlewares/validateCampos.js";
+import { loginAuthentication } from "../middlewares/loginAuthentication.js";
+import { verifySignUp } from  "../middlewares/verifySignup.js";
+
+
+const router = Router();
+
+
+router.get('/login', loginUser);
+router.get('/users', getAllUsers);
+
+
+router.post('/register' , [
+    check('name', 'debe contener nombre, ').isLength({min: 4}),
+    check('age', 'debe contener dos numeros').isLength({max: 2}),
+    check('email', 'el mail debe ser correcto').isEmail().not().isEmpty(),
+    validarCampos, verifySignUp, 
+], createUser)
+
+
+//peticion POST login verificacion credenciales
+router.post('/login', [
+    check('email', 'El Email es requerido.').isEmail(),
+    check('password', 'La contraseña es requerida.').not().isEmpty(),
+    validarCampos, 
+    loginAuthentication,
+    loginUser,
+], async (req, res) => {
+    loginUser (req, res);
+    
+});
+    
+
+
+
+
+export default router
